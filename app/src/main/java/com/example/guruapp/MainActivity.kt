69c28ebbe1688bb6d15d693594b12e4376563d4a
wlsskdbwls <wlsskdbwls@naver.com>
2021-07-26 18:39:43 +0900

    commit

---
+++ app/src/main/java/com/example/guruapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.guruapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_reg, menu)
+        return true
+    }
+}
